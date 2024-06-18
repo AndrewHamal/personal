86e0c5b576b3c3cee1785f1e26236abc09b94a2a
AndrewHamal <73131641+AndrewHamal@users.noreply.github.com>
2024-06-18 17:27:01 +0545

    fixed

--- pages/homeScreen/HomeScreen.tsx
+++ pages/homeScreen/HomeScreen.tsx
@@ -448,6 +448,7 @@ const HomeScreen = ({ setShowSider }: any) => {
                 </video>
               </div>
             </div>
+
             <div className="mt-4 w-[100%]  flex-column" data-aos="fade-right">
               <h3 className="mb-3 text-blue-500">Brighter Future</h3>
               <div className="">
@@ -458,6 +459,20 @@ const HomeScreen = ({ setShowSider }: any) => {
                   }}
                   controls={false} autoPlay muted loop={true}>
                   <source src="/videos/brighter.mp4" type="video/mp4" />
+                </video>
+              </div>
+            </div>
+
+            <div className="mt-4 w-[100%]  flex-column" data-aos="fade-right">
+              <h3 className="mb-3 text-blue-500">Brighter Future</h3>
+              <div className="">
+                <video
+                  style={{
+                    objectFit: 'contain',
+                    width: '100%'
+                  }}
+                  controls={false} autoPlay muted loop={true}>
+                  <source src="/videos/sales.mp4" type="video/mp4" />
                 </video>
               </div>
             </div>

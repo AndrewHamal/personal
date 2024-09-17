a1a2b87ddc31938b0985020a8d19b9da2ba73bf8
Anis Hamal <73131641+AndrewHamal@users.noreply.github.com>
2024-09-17 13:57:56 +0545

    fixed

--- pages/homeScreen/HomeScreen.tsx
+++ pages/homeScreen/HomeScreen.tsx
@@ -456,6 +456,18 @@ const HomeScreen = ({ setShowSider }: any) => {
           </div>
 
           <div className="flex flex-wrap justify-between gap-4 mt-20 h-100">
+          <div className="w-[100%] " data-aos="fade-right">
+              <h3 className="mb-3 text-blue-500">Grihabhumi</h3>
+              <div className="mx-auto overflow-hidden">
+                <video
+                  style={{
+                    objectFit: 'contain',
+                    width: '100%'
+                  }} controls={true} muted>
+                  <source src="https://quiptgsm.com/bg-1.mov" type="video/mp4" />
+                </video>
+              </div>
+            </div>
             <div className="w-[100%] " data-aos="fade-right">
               <h3 className="mb-3 text-blue-500">Grihabhumi</h3>
               <div className="mx-auto overflow-hidden">

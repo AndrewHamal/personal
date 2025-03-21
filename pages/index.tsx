30ec23613c19f509aad1316aba2178caceeddb92
Anis Hamal <73131641+AndrewHamal@users.noreply.github.com>
2025-03-21 19:42:38 +0545

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,18 @@ export default function Home() {
     <>
         <Head>
           <title>❤️ from Anis Hamal</title>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,i) { 
+              w._captiwateSettings={site:i};
+              var h=d.getElementsByTagName('head')[0];
+              var s=d.createElement('script');
+              s.async=1; s.type='module';
+              s.src='https://widget.captiwate.com/widget.js';
+              h.appendChild(s);
+            }) (window, document, '30251d3c-9ad9-4f00-a852-54f17c76cebb');`
+          }}
+        />
         </Head>
         <Drawer width={(window.innerWidth > 800 && window.innerWidth < 900) ? "60%" : window.innerWidth > 900 ? '50%' : "100%"} title={<img  className="ml-auto rounded-full" src="/img/logo.svg" width={80} alt="" />} closeIcon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
           <path d="M24 12C24 18.6094 18.6563 24 12 24C5.39063 24 1.25807e-06 18.6094 1.83588e-06 12C2.41779e-06 5.34375 5.39063 -1.62688e-06 12 -1.04907e-06C18.6563 -4.67165e-07 24 5.34375 24 12ZM15.7969 14.2031L13.5938 12L15.7969 9.79687C16.2656 9.32812 16.2656 8.625 15.7969 8.20312C15.375 7.73437 14.6719 7.73437 14.25 8.20312L12.0469 10.4062L9.79688 8.20312C9.375 7.73437 8.67188 7.73437 8.25 8.20312C7.78125 8.625 7.78125 9.32812 8.25 9.79687L10.4531 12L8.25 14.2031C7.78125 14.625 7.78125 15.3281 8.25 15.7969C8.67188 16.2187 9.375 16.2187 9.79688 15.7969L12.0469 13.5469L14.25 15.7969C14.6719 16.2187 15.375 16.2187 15.7969 15.7969C16.2656 15.3281 16.2656 14.625 15.7969 14.2031Z" fill="#030128"/>
